2f79a34213979acbb70c693da65350a9c3658d51
Kurt <Kurt@10.4.2.64>
2016-01-04 11:33:21 -0600

    Starter Code

---
+++ Elevens/src/elevens/Elevens.java
@@ -0,0 +1,16 @@
+package elevens;
+
+/**
+ *
+ * @author Kurt
+ */
+public class Elevens {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
